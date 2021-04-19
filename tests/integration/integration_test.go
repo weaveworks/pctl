61fe9f0dfab396dac61074fde2d27bba4383b1db
cpu1 <patwal.chetan@gmail.com>
2021-04-19 16:51:45 +0530

    Fix integration tests

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -11,33 +11,29 @@ import (
 )
 
 var _ = Describe("PCTL", func() {
-	var exampleCatalog string
-	BeforeEach(func() {
-		exampleCatalog = "http://localhost:8080"
-	})
 
 	Context("search", func() {
 		It("returns the matching profiles", func() {
-			cmd := exec.Command(binaryPath, "--catalog-url", exampleCatalog, "search", "nginx")
+			cmd := exec.Command(binaryPath, "search", "nginx")
 			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Expect(err).ToNot(HaveOccurred())
 			Eventually(session).Should(gexec.Exit(0))
 			Expect(string(session.Out.Contents())).To(ContainSubstring("weaveworks-nginx: This installs nginx"))
 		})
 
-		When("catalog-url is not provided", func() {
+		When("kubeconfig is incorrectly set", func() {
 			It("returns a useful error", func() {
-				cmd := exec.Command(binaryPath, "search", "nginx")
+				cmd := exec.Command(binaryPath, "--kubeconfig=/non-existing/path/kubeconfig", "search", "nginx")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).ToNot(HaveOccurred())
 				Eventually(session).Should(gexec.Exit(1))
-				Expect(string(session.Err.Contents())).To(ContainSubstring("--catalog-url or $PCTL_CATALOG_URL must be provided"))
+				Expect(string(session.Err.Contents())).To(ContainSubstring("failed to create config from kubeconfig path"))
 			})
 		})
 
 		When("a search string is not provided", func() {
 			It("returns a useful error", func() {
-				cmd := exec.Command(binaryPath, "--catalog-url", exampleCatalog, "search")
+				cmd := exec.Command(binaryPath, "search")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).ToNot(HaveOccurred())
 				Eventually(session).Should(gexec.Exit(1))
@@ -48,7 +44,7 @@ var _ = Describe("PCTL", func() {
 
 	Context("show", func() {
 		It("returns information about the given profile", func() {
-			cmd := exec.Command(binaryPath, "--catalog-url", exampleCatalog, "show", "nginx-catalog/weaveworks-nginx")
+			cmd := exec.Command(binaryPath, "show", "nginx-catalog/weaveworks-nginx")
 			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Expect(err).ToNot(HaveOccurred())
 			Eventually(session).Should(gexec.Exit(0))
@@ -58,12 +54,12 @@ var _ = Describe("PCTL", func() {
 			Expect(string(session.Out.Contents())).To(ContainSubstring("version: 0.0.1"))
 			Expect(string(session.Out.Contents())).To(ContainSubstring("catalog: nginx-catalog"))
 			Expect(string(session.Out.Contents())).To(ContainSubstring("prerequisites:\n- Kubernetes 1.18+"))
-			Expect(string(session.Out.Contents())).To(ContainSubstring("maintainer: WeaveWorks <gitops@weave.works>"))
+			Expect(string(session.Out.Contents())).To(ContainSubstring("maintainer: weaveworks (https://github.com/weaveworks/profiles)"))
 		})
 
 		When("the profile is not listed in the catalog", func() {
 			It("returns a useful error", func() {
-				cmd := exec.Command(binaryPath, "--catalog-url", exampleCatalog, "show", "foo/unlisted")
+				cmd := exec.Command(binaryPath, "show", "foo/unlisted")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).ToNot(HaveOccurred())
 				Eventually(session).Should(gexec.Exit(1))
@@ -71,19 +67,9 @@ var _ = Describe("PCTL", func() {
 			})
 		})
 
-		When("catalog-url is not provided", func() {
-			It("returns a useful error", func() {
-				cmd := exec.Command(binaryPath, "show", "weaveworks-nginx")
-				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).ToNot(HaveOccurred())
-				Eventually(session).Should(gexec.Exit(1))
-				Expect(string(session.Err.Contents())).To(ContainSubstring("--catalog-url or $PCTL_CATALOG_URL must be provided"))
-			})
-		})
-
 		When("a name argument is not provided", func() {
 			It("returns a useful error", func() {
-				cmd := exec.Command(binaryPath, "--catalog-url", exampleCatalog, "show")
+				cmd := exec.Command(binaryPath, "show")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).ToNot(HaveOccurred())
 				Eventually(session).Should(gexec.Exit(1))
