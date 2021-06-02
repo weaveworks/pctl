aef369192fa91452c70da59eb3b4b72b9f579e19
Gergely Brautigam <skarlso777@gmail.com>
2021-06-02 11:00:10 +0200

    Fix the integration test

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -214,21 +214,44 @@ var _ = Describe("PCTL", func() {
 			))
 		})
 
-		//When("there are no available updates", func() {
-		//	It("returns the subscriptions", func() {
-		//		listCmd := func() []string {
-		//			cmd := exec.Command(binaryPath, "list")
-		//			session, err := cmd.CombinedOutput()
-		//			Expect(err).ToNot(HaveOccurred())
-		//			return strings.Split(string(session), "\n")
-		//		}
-		//
-		//		Eventually(listCmd).Should(ContainElements(
-		//			"NAMESPACE\tNAME                       \tSOURCE                                   \tAVAILABLE UPDATES ",
-		//			"default  \tlong-name-to-ensure-padding\tfoo/multi-version-weaveworks-nginx/v0.1.0\tv0.1.1           \t",
-		//		))
-		//	})
-		//})
+		When("there are no available updates", func() {
+			It("returns the subscriptions", func() {
+				profileURL := "https://github.com/weaveworks/profiles-examples"
+				bitnamiSub := profilesv1.ProfileSubscription{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "ProfileSubscription",
+						APIVersion: "profilesubscriptions.weave.works/v1alpha1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "bitnami-profile",
+						Namespace: namespace,
+					},
+					Spec: profilesv1.ProfileSubscriptionSpec{
+						ProfileURL: profileURL,
+						Version:    "bitnami-nginx/v0.1.0",
+						ProfileCatalogDescription: &profilesv1.ProfileCatalogDescription{
+							Catalog: "nginx-catalog",
+							Profile: "bitnami-nginx",
+							Version: "v0.1.0",
+						},
+					},
+				}
+				Expect(kClient.Create(ctx, &bitnamiSub)).Should(Succeed())
+				listCmd := func() []string {
+					cmd := exec.Command(binaryPath, "list")
+					session, err := cmd.CombinedOutput()
+					Expect(err).ToNot(HaveOccurred())
+					return strings.Split(string(session), "\n")
+				}
+
+				Eventually(listCmd).Should(ContainElements(
+					"NAMESPACE\tNAME                       \tSOURCE                               \tAVAILABLE UPDATES ",
+					"default  \tbitnami-profile            \tnginx-catalog/bitnami-nginx/v0.1.0   \t-                \t",
+					"default  \tlong-name-to-ensure-padding\tnginx-catalog/weaveworks-nginx/v0.1.0\tv0.1.1           \t",
+				))
+				Expect(kClient.Delete(ctx, &bitnamiSub)).Should(Succeed())
+			})
+		})
 	})
 
 	Context("install", func() {
