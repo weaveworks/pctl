9ba817e2cc70d9d362ba7005a5b86bb0e071b986
Himangini <its_himangini@yahoo.com>
2021-07-28 15:47:43 +0100

    fixed failing integration tests

--- tests/integration/integration_version_test.go
+++ tests/integration/integration_version_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/weaveworks/pctl/pkg/version"
 )
 
 var _ = Describe("pctl list", func() {
@@ -13,14 +14,14 @@ var _ = Describe("pctl list", func() {
 			cmd := exec.Command(binaryPath, "--version")
 			session, err := cmd.CombinedOutput()
 			Expect(err).ToNot(HaveOccurred())
-			Expect(string(session)).To(ContainSubstring("0.1.0"))
+			Expect(string(session)).To(ContainSubstring(version.Version))
 		})
 
 		It("returns information about the version of pctl with shorthand flag", func() {
 			cmd := exec.Command(binaryPath, "-v")
 			session, err := cmd.CombinedOutput()
 			Expect(err).ToNot(HaveOccurred())
-			Expect(string(session)).To(ContainSubstring("0.1.0"))
+			Expect(string(session)).To(ContainSubstring(version.Version))
 		})
 	})
 })
