e91a728e3ed8faa835be071fbc497d81530efe35
Himangini <its_himangini@yahoo.com>
2021-08-02 14:30:23 +0100

    updated list_test.go

--- pkg/catalog/list_test.go
+++ pkg/catalog/list_test.go
@@ -38,6 +38,7 @@ var _ = Describe("List", func() {
 		profile3   = "weaveworks-nginx-3"
 		cat        = "nginx-catalog"
 		version    = "v0.1.0"
+		url        = "https://github.com/org/repo-name"
 	)
 
 	BeforeEach(func() {
@@ -53,7 +54,7 @@ var _ = Describe("List", func() {
 			},
 			Spec: profilesv1.ProfileInstallationSpec{
 				Source: &profilesv1.Source{
-					URL: "https://github.com/org/repo-name",
+					URL: url,
 					Tag: "weaveworks-nginx/v0.1.0",
 				},
 				Catalog: &profilesv1.Catalog{
@@ -87,20 +88,57 @@ var _ = Describe("List", func() {
 		expected := []catalog.ProfileData{
 			{
 				Profile: installation.Summary{
-					Name:      "sub1",
-					Namespace: "default",
-					Profile:   "weaveworks-nginx",
-					Catalog:   "nginx-catalog",
+					Name:      sub1,
+					Namespace: namespace1,
+					Profile:   profile,
+					Catalog:   cat,
 					Branch:    "-",
 					Path:      "-",
-					Version:   "v0.1.0",
-					URL:       "https://github.com/org/repo-name",
+					Version:   version,
+					URL:       url,
 				},
 				AvailableVersionUpdates: []string{"v0.1.1"},
 			},
 		}
 		Expect(out).To(Equal(expected))
 	})
+
+	It("lists installed profiles and checks for updates to profiles with name", func() {
+		httpBody := []byte(`{"items":[
+  {
+    "name": "weaveworks-nginx",
+    "description": "This installs the next version nginx.",
+    "tag": "v0.1.1",
+    "catalogSource": "nginx-catalog",
+    "url": "https://github.com/weaveworks/profiles-examples",
+    "maintainer": "weaveworks (https://github.com/weaveworks/profiles)",
+    "prerequisites": [
+      "Kubernetes 1.18+"
+    ]
+  }
+]}
+`)
+		fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
+		out, err := manager.List(fakeRuntimeClient, fakeCatalogClient, sub1)
+		Expect(err).NotTo(HaveOccurred())
+		expected := []catalog.ProfileData{
+			{
+				Profile: installation.Summary{
+					Name:      sub1,
+					Namespace: namespace1,
+					Profile:   profile,
+					Catalog:   cat,
+					Branch:    "-",
+					Path:      "-",
+					Version:   version,
+					URL:       url,
+				},
+				AvailableVersionUpdates: []string{"v0.1.1"},
+			},
+		}
+		Expect(out).To(Equal(expected))
+	})
+
 	When("there are no profiles", func() {
 		It("returns an empty list with no error", func() {
 			scheme := runtime.NewScheme()
@@ -110,7 +148,16 @@ var _ = Describe("List", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(out).To(BeEmpty())
 		})
+		It("returns an empty list with no error when a name is supplied", func() {
+			scheme := runtime.NewScheme()
+			Expect(profilesv1.AddToScheme(scheme)).To(Succeed())
+			fakeRuntimeClient = fake.NewClientBuilder().WithScheme(scheme).Build()
+			out, err := manager.List(fakeRuntimeClient, fakeCatalogClient, "testProfileName")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(out).To(BeEmpty())
+		})
 	})
+
 	When("there are no updates for a profile", func() {
 		It("returns the profile without any version updates", func() {
 			httpBody := []byte(`{"items":[]}`)
@@ -120,14 +167,36 @@ var _ = Describe("List", func() {
 			expected := []catalog.ProfileData{
 				{
 					Profile: installation.Summary{
-						Name:      "sub1",
-						Namespace: "default",
-						Profile:   "weaveworks-nginx",
-						Catalog:   "nginx-catalog",
+						Name:      sub1,
+						Namespace: namespace1,
+						Profile:   profile,
+						Catalog:   cat,
 						Branch:    "-",
 						Path:      "-",
-						Version:   "v0.1.0",
-						URL:       "https://github.com/org/repo-name",
+						Version:   version,
+						URL:       url,
+					},
+					AvailableVersionUpdates: []string{"-"},
+				},
+			}
+			Expect(out).To(Equal(expected))
+		})
+		It("returns the profile without any version updates with name", func() {
+			httpBody := []byte(`{"items":[]}`)
+			fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
+			out, err := manager.List(fakeRuntimeClient, fakeCatalogClient, sub1)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []catalog.ProfileData{
+				{
+					Profile: installation.Summary{
+						Name:      sub1,
+						Namespace: namespace1,
+						Profile:   profile,
+						Catalog:   cat,
+						Branch:    "-",
+						Path:      "-",
+						Version:   version,
+						URL:       url,
 					},
 					AvailableVersionUpdates: []string{"-"},
 				},
@@ -144,6 +213,13 @@ var _ = Describe("List", func() {
 			Expect(err.Error()).To(Equal("failed to get available updates: failed to fetch available updates for profile, status code 400"))
 			Expect(out).To(BeNil())
 		})
+		It("returns a sane error when name is provided", func() {
+			fakeCatalogClient.DoRequestReturns(nil, 400, nil)
+			out, err := manager.List(fakeRuntimeClient, fakeCatalogClient, sub1)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("failed to get available updates: failed to fetch available updates for profile, status code 400"))
+			Expect(out).To(BeNil())
+		})
 	})
 
 	When("kubernetes list fails", func() {
@@ -151,6 +227,12 @@ var _ = Describe("List", func() {
 			fakeRuntimeClient = fake.NewClientBuilder().Build()
 			fakeCatalogClient.DoRequestReturns(nil, 200, nil)
 			_, err := manager.List(fakeRuntimeClient, fakeCatalogClient, "")
+			Expect(err).To(MatchError("failed to list profile installations: no kind is registered for the type v1alpha1.ProfileInstallationList in scheme \"pkg/runtime/scheme.go:100\""))
+		})
+		It("returns a sane error when name is provided", func() {
+			fakeRuntimeClient = fake.NewClientBuilder().Build()
+			fakeCatalogClient.DoRequestReturns(nil, 200, nil)
+			_, err := manager.List(fakeRuntimeClient, fakeCatalogClient, sub1)
 			Expect(err).To(MatchError("failed to list profile installations: no kind is registered for the type v1alpha1.ProfileInstallationList in scheme \"pkg/runtime/scheme.go:100\""))
 		})
 	})
@@ -165,7 +247,7 @@ var _ = Describe("List", func() {
 				},
 				Spec: profilesv1.ProfileInstallationSpec{
 					Source: &profilesv1.Source{
-						URL: "https://github.com/org/repo-name",
+						URL: url,
 						Tag: "weaveworks-nginx-2/v0.1.0",
 					},
 					Catalog: &profilesv1.Catalog{
@@ -183,7 +265,7 @@ var _ = Describe("List", func() {
 				},
 				Spec: profilesv1.ProfileInstallationSpec{
 					Source: &profilesv1.Source{
-						URL: "https://github.com/org/repo-name",
+						URL: url,
 						Tag: "weaveworks-nginx-3/v0.1.0",
 					},
 					Catalog: &profilesv1.Catalog{
@@ -233,40 +315,145 @@ var _ = Describe("List", func() {
 			expected := []catalog.ProfileData{
 				{
 					Profile: installation.Summary{
-						Name:      "sub1",
-						Namespace: "default",
-						Profile:   "weaveworks-nginx",
-						Catalog:   "nginx-catalog",
+						Name:      sub1,
+						Namespace: namespace1,
+						Profile:   profile,
+						Catalog:   cat,
 						Branch:    "-",
 						Path:      "-",
-						Version:   "v0.1.0",
-						URL:       "https://github.com/org/repo-name",
+						Version:   version,
+						URL:       url,
 					},
 					AvailableVersionUpdates: []string{"v0.1.1"},
 				},
 				{
 					Profile: installation.Summary{
-						Name:      "sub2",
-						Namespace: "namespace2",
-						Profile:   "weaveworks-nginx-2",
-						Catalog:   "nginx-catalog",
+						Name:      sub2,
+						Namespace: namespace2,
+						Profile:   profile2,
+						Catalog:   cat,
 						Branch:    "-",
 						Path:      "-",
-						Version:   "v0.1.0",
-						URL:       "https://github.com/org/repo-name",
+						Version:   version,
+						URL:       url,
 					},
 					AvailableVersionUpdates: []string{"-"},
 				},
 				{
 					Profile: installation.Summary{
-						Name:      "sub3",
-						Namespace: "namespace3",
-						Profile:   "weaveworks-nginx-3",
-						Catalog:   "nginx-catalog",
+						Name:      sub3,
+						Namespace: namespace3,
+						Profile:   profile3,
+						Catalog:   cat,
 						Branch:    "-",
 						Path:      "-",
-						Version:   "v0.1.0",
-						URL:       "https://github.com/org/repo-name",
+						Version:   version,
+						URL:       url,
+					},
+					AvailableVersionUpdates: []string{"v0.1.5"},
+				},
+			}
+			Expect(out).To(Equal(expected))
+		})
+
+		It("returns a proper result which matches name when there are multiple installed profiles", func() {
+			pSub2 := &profilesv1.ProfileInstallation{
+				TypeMeta: profileTypeMeta,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      sub3,
+					Namespace: namespace2,
+				},
+				Spec: profilesv1.ProfileInstallationSpec{
+					Source: &profilesv1.Source{
+						URL: url,
+						Tag: "weaveworks-nginx-2/v0.1.0",
+					},
+					Catalog: &profilesv1.Catalog{
+						Profile: profile2,
+						Catalog: cat,
+						Version: version,
+					},
+				},
+			}
+			pSub3 := &profilesv1.ProfileInstallation{
+				TypeMeta: profileTypeMeta,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      sub3,
+					Namespace: namespace3,
+				},
+				Spec: profilesv1.ProfileInstallationSpec{
+					Source: &profilesv1.Source{
+						URL: url,
+						Tag: "weaveworks-nginx-3/v0.1.0",
+					},
+					Catalog: &profilesv1.Catalog{
+						Profile: profile3,
+						Catalog: cat,
+						Version: version,
+					},
+				},
+			}
+			Expect(fakeRuntimeClient.Create(context.TODO(), pSub2)).To(Succeed())
+			Expect(fakeRuntimeClient.Create(context.TODO(), pSub3)).To(Succeed())
+			return1 := []byte(`{"items":[
+  {
+    "name": "weaveworks-nginx",
+    "description": "This installs the next version nginx.",
+    "tag": "v0.1.1",
+    "catalogSource": "nginx-catalog",
+    "url": "https://github.com/weaveworks/profiles-examples",
+    "maintainer": "weaveworks (https://github.com/weaveworks/profiles)",
+    "prerequisites": [
+      "Kubernetes 1.18+"
+    ]
+  }
+]}
+`)
+
+			return2 := []byte(`{"items":[]}`)
+			return3 := []byte(`{"items":[
+  {
+    "name": "weaveworks-nginx-3",
+    "description": "This installs nginx.",
+    "tag": "v0.1.5",
+    "catalogSource": "nginx-catalog",
+    "url": "https://github.com/weaveworks/profiles-examples",
+    "maintainer": "weaveworks (https://github.com/weaveworks/profiles)",
+    "prerequisites": [
+      "Kubernetes 1.18+"
+    ]
+  }
+]}
+`)
+			fakeCatalogClient.DoRequestReturnsOnCall(0, return1, 200, nil)
+			fakeCatalogClient.DoRequestReturnsOnCall(1, return2, 200, nil)
+			fakeCatalogClient.DoRequestReturnsOnCall(2, return3, 200, nil)
+			out, err := manager.List(fakeRuntimeClient, fakeCatalogClient, sub3)
+			Expect(err).NotTo(HaveOccurred())
+			expected := []catalog.ProfileData{
+				{
+					Profile: installation.Summary{
+						Name:      sub3,
+						Namespace: namespace2,
+						Profile:   profile2,
+						Catalog:   cat,
+						Branch:    "-",
+						Path:      "-",
+						Version:   version,
+						URL:       url,
+					},
+					AvailableVersionUpdates: []string{"-"},
+				},
+				{
+					Profile: installation.Summary{
+						Name:      sub3,
+						Namespace: namespace3,
+						Profile:   profile3,
+						Catalog:   cat,
+						Branch:    "-",
+						Path:      "-",
+						Version:   version,
+						URL:       url,
 					},
 					AvailableVersionUpdates: []string{"v0.1.5"},
 				},
