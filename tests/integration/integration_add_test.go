9d33b864d4e24d745d9ea822ce2f74b6568cb73c
nikimanoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2021-08-06 18:25:44 +0300

    Replace --profile-url with --profile-repo-url - Replacing because --profile-repo-url is better at conveying that it refers to the URL needed to clone the profile, not the full URL that leads to the profile.yaml itself.

--- tests/integration/integration_add_test.go
+++ tests/integration/integration_add_test.go
@@ -121,7 +121,7 @@ var _ = Describe("pctl add", func() {
 				subName,
 				"--profile-branch",
 				profileBranch,
-				"--profile-url", profileExamplesURL,
+				"--profile-repo-url", profileExamplesURL,
 				"--profile-path", "weaveworks-nginx",
 				"--config-map", configMapName)
 			cmd.Dir = temp
@@ -365,7 +365,7 @@ status: {}
 					namespace+"/git-repo-name",
 					"--namespace",
 					namespace,
-					"--profile-url",
+					"--profile-repo-url",
 					profileExamplesURL,
 					"--profile-branch",
 					branch,
@@ -423,7 +423,7 @@ status: {}
 				namespace := uuid.New().String()
 				branch := "main"
 				path := "bitnami-nginx"
-				cmd := exec.Command(binaryPath, "add", "--out", temp, "--git-repository", namespace+"/git-repo-name", "--namespace", namespace, "--profile-url", pctlPrivateProfilesRepositoryName, "--profile-branch", branch, "--profile-path", path)
+				cmd := exec.Command(binaryPath, "add", "--out", temp, "--git-repository", namespace+"/git-repo-name", "--namespace", namespace, "--profile-repo-url", pctlPrivateProfilesRepositoryName, "--profile-branch", branch, "--profile-path", path)
 				cmd.Dir = temp
 
 				if v := os.Getenv("PRIVATE_EXAMPLES_DEPLOY_KEY"); v != "" {
@@ -481,7 +481,7 @@ status: {}
 					namespace+"/git-repo-name",
 					"--namespace",
 					namespace,
-					"--profile-url",
+					"--profile-repo-url",
 					profileExamplesURL,
 					"--profile-branch",
 					branch,
