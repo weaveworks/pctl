d7dfb02088e835a15069485f6f2e63ce56e99606
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-09-14 16:21:09 +0200

    Added lots of testing

--- api/add_test.go
+++ api/add_test.go
@@ -36,11 +36,11 @@ var _ = Describe("AddProfile", func() {
 		  `)
 		fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
 		fakeGit.CloneStub = func(s string, s2 string, s3 string) error {
+			Expect(s).To(Equal("https://github.com/weaveworks/nginx-profile"))
+			Expect(s2).To(Equal("0.0.1"))
 			content, err := ioutil.ReadFile(filepath.Join(".", "testdata", "profile.yaml"))
 			Expect(err).ToNot(HaveOccurred())
-			profile, err := os.Create(filepath.Join(s3, "profile.yaml"))
-			Expect(err).ToNot(HaveOccurred())
-			_, err = profile.Write(content)
+			err = ioutil.WriteFile(filepath.Join(s3, "profile.yaml"), content, 0644)
 			Expect(err).ToNot(HaveOccurred())
 			return nil
 		}
@@ -58,5 +58,182 @@ var _ = Describe("AddProfile", func() {
 
 		Expect(err).NotTo(HaveOccurred())
 		Expect(installationPath).To(Equal(filepath.Join("test-dir", "sub-name")))
+	})
+	When("a URL is provided", func() {
+		It("will use that to install a profile", func() {
+			fakeGit = &gitfake.FakeGit{}
+			fakeCatalogClient = &catalogfake.FakeCatalogClient{}
+		})
+		It("can retrieve profiles", func() {
+			httpBody := []byte(`{"item":
+{
+	"name": "nginx-1",
+	"description": "nginx 1",
+	"tag": "0.0.1",
+	"catalogSource": "weaveworks (https://github.com/weaveworks/profiles)",
+	"url": "https://github.com/weaveworks/nginx-profile",
+	"prerequisites": ["Kubernetes 1.18+"],
+	"maintainer": "WeaveWorks <gitops@weave.works>"
+}}
+		  `)
+			fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
+			fakeGit.CloneStub = func(s string, s2 string, s3 string) error {
+				Expect(s).To(Equal("https://github.com/org/repo"))
+				Expect(s2).To(Equal("main"))
+				content, err := ioutil.ReadFile(filepath.Join(".", "testdata", "profile.yaml"))
+				Expect(err).ToNot(HaveOccurred())
+				err = ioutil.WriteFile(filepath.Join(s3, "profile.yaml"), content, 0644)
+				Expect(err).ToNot(HaveOccurred())
+				return nil
+			}
+			installationPath, err := api.AddProfile(api.AddProfileOpts{
+				Branch:        "main",
+				SubName:       "sub-name",
+				Namespace:     "namespace",
+				ConfigMap:     "configMap",
+				Dir:           "test-dir",
+				GitRepository: "git-repo-namespace/git-repo-name",
+				URL:           "https://github.com/org/repo",
+				CatalogClient: fakeCatalogClient,
+				GitClient:     fakeGit,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(installationPath).To(Equal(filepath.Join("test-dir", "sub-name")))
+		})
+		When("invalid profile path is provided", func() {
+			It("returns an error", func() {
+				_, err := api.AddProfile(api.AddProfileOpts{
+					Branch:        "main",
+					SubName:       "sub-name",
+					Namespace:     "namespace",
+					ConfigMap:     "configMap",
+					Dir:           "test-dir",
+					GitRepository: "git-repo-namespace/git-repo-name",
+					ProfilePath:   "invalid",
+					CatalogClient: fakeCatalogClient,
+					GitClient:     fakeGit,
+				})
+
+				Expect(err).To(MatchError("both catalog name and profile name must be provided"))
+			})
+		})
+		When("path and url are provided", func() {
+			It("uses that", func() {
+				httpBody := []byte(`{"item":
+{
+	"name": "nginx-1",
+	"description": "nginx 1",
+	"tag": "0.0.1",
+	"catalogSource": "weaveworks (https://github.com/weaveworks/profiles)",
+	"url": "https://github.com/weaveworks/nginx-profile",
+	"prerequisites": ["Kubernetes 1.18+"],
+	"maintainer": "WeaveWorks <gitops@weave.works>"
+}}
+		  `)
+				fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
+				fakeGit.CloneStub = func(s string, s2 string, s3 string) error {
+					Expect(s).To(Equal("https://github.com/org/repo"))
+					Expect(s2).To(Equal("main"))
+					content, err := ioutil.ReadFile(filepath.Join(".", "testdata", "profile.yaml"))
+					Expect(err).ToNot(HaveOccurred())
+					_ = os.MkdirAll(filepath.Join(s3, "path"), 0700)
+					err = ioutil.WriteFile(filepath.Join(s3, "path", "profile.yaml"), content, 0644)
+					Expect(err).ToNot(HaveOccurred())
+					return nil
+				}
+				installationPath, err := api.AddProfile(api.AddProfileOpts{
+					Branch:        "main",
+					SubName:       "sub-name",
+					Namespace:     "namespace",
+					ConfigMap:     "configMap",
+					Dir:           "test-dir",
+					GitRepository: "git-repo-namespace/git-repo-name",
+					URL:           "https://github.com/org/repo",
+					Path:          "path",
+					CatalogClient: fakeCatalogClient,
+					GitClient:     fakeGit,
+				})
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(installationPath).To(Equal(filepath.Join("test-dir", "sub-name")))
+			})
+		})
+		When("profile path includes a version", func() {
+			It("uses that to query the profile manager", func() {
+				httpBody := []byte(`{"item":
+{
+	"name": "nginx-1",
+	"description": "nginx 1",
+	"tag": "0.0.1",
+	"catalogSource": "weaveworks (https://github.com/weaveworks/profiles)",
+	"url": "https://github.com/weaveworks/nginx-profile",
+	"prerequisites": ["Kubernetes 1.18+"],
+	"maintainer": "WeaveWorks <gitops@weave.works>"
+}}
+		  `)
+				fakeCatalogClient.DoRequestReturns(httpBody, 200, nil)
+				fakeGit.CloneStub = func(s string, s2 string, s3 string) error {
+					Expect(s).To(Equal("https://github.com/weaveworks/nginx-profile"))
+					Expect(s2).To(Equal("0.0.1"))
+					content, err := ioutil.ReadFile(filepath.Join(".", "testdata", "profile.yaml"))
+					Expect(err).ToNot(HaveOccurred())
+					err = ioutil.WriteFile(filepath.Join(s3, "profile.yaml"), content, 0644)
+					Expect(err).ToNot(HaveOccurred())
+					return nil
+				}
+				installationPath, err := api.AddProfile(api.AddProfileOpts{
+					Branch:        "main",
+					SubName:       "sub-name",
+					Namespace:     "namespace",
+					ConfigMap:     "configMap",
+					Dir:           "test-dir",
+					GitRepository: "git-repo-namespace/git-repo-name",
+					ProfilePath:   "catalog/profile/version",
+					CatalogClient: fakeCatalogClient,
+					GitClient:     fakeGit,
+				})
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(installationPath).To(Equal(filepath.Join("test-dir", "sub-name")))
+				arg, _ := fakeCatalogClient.DoRequestArgsForCall(0)
+				Expect(arg).To(Equal("/profiles/catalog/profile/version"))
+			})
+		})
+
+		When("git repository is invalid", func() {
+			It("returns an error", func() {
+				_, err := api.AddProfile(api.AddProfileOpts{
+					Branch:        "main",
+					SubName:       "sub-name",
+					Namespace:     "namespace",
+					ConfigMap:     "configMap",
+					Dir:           "test-dir",
+					GitRepository: "invalid",
+					URL:           "https://github.com/org/repo",
+					Path:          "path",
+					CatalogClient: fakeCatalogClient,
+					GitClient:     fakeGit,
+				})
+
+				Expect(err).To(MatchError("git-repository must in format <namespace>/<name>; was: invalid"))
+			})
+		})
+		When("git repository is no provided", func() {
+			It("will try and look into a local config file", func() {
+				_, err := api.AddProfile(api.AddProfileOpts{
+					Branch:        "main",
+					SubName:       "sub-name",
+					Namespace:     "namespace",
+					ConfigMap:     "configMap",
+					Dir:           "test-dir",
+					URL:           "https://github.com/org/repo",
+					Path:          "path",
+					CatalogClient: fakeCatalogClient,
+					GitClient:     fakeGit,
+				})
+				Expect(err).To(MatchError("flux git repository not provided, please provide the --git-repository flag or use the pctl bootstrap functionality"))
+			})
+		})
 	})
 })
